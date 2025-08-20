0a10609dcb4148908fd9103fc72cfbddf6ed988b
Christina Marikos <christina.marikos@cta-tech.app>
2025-08-20 20:41:54 +0000

    init protect our pueblos

---
+++ protect_our_pueblos.sql
@@ -0,0 +1,12 @@
+SELECT
+e.event_name
+
+FROM `proj-tmc-mem-mvp.everyaction_enhanced.enh_everyaction__events` AS e
+
+LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__eventscodes` AS ec
+  ON e.eventid = ec.eventid 
+
+LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__codes` AS c
+  ON ec.codeid = c.codeid
+
+WHERE c.codename = 'Protect our Pueblos'
