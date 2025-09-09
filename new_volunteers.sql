effda1dd58b409425c98f8cb0d1a71194bb68b74
Christina Marikos <christina.marikos@cta-tech.app>
2025-09-09 22:01:47 +0000

    update new vols

--- new_volunteers.sql
+++ new_volunteers.sql
@@ -1,3 +1,5 @@
+CREATE OR REPLACE VIEW `prod-organize-arizon-4e1c0a83.organizing_view.new_volunteers` AS(
+WITH volunteers AS(
 SELECT DISTINCT
 e.vanid
 , c.codename AS organizer
@@ -16,4 +18,39 @@ AND c.codename IN ('Jhanitzel Bogarin','Leny Rivera','Tara Clayton','Hector Cast
 
 GROUP BY 1,2
 ORDER BY 3
+)
 
+, attendees AS(
+SELECT DISTINCT
+e.vanid
+, c.codename AS organizer
+, MIN(EXTRACT(MONTH FROM e.start_date)) AS first_month
+FROM `proj-tmc-mem-mvp.everyaction_enhanced.enh_everyaction__events` AS e
+
+LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__eventscodes` AS ec
+  ON e.eventid = ec.eventid 
+
+LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__codes` AS c
+  ON ec.codeid = c.codeid
+
+WHERE e.role = 'Attendee'
+and e.final_status = 'Completed'
+AND c.codename IN ('Jhanitzel Bogarin','Leny Rivera','Tara Clayton','Hector Castellanos','Elyanna Juarez')
+
+GROUP BY 1,2
+ORDER BY 3
+)
+
+SELECT
+a.vanid
+, a.organizer
+, a.first_month AS fist_event
+, v.first_month AS first_vol
+FROM attendees AS a
+
+
+LEFT JOIN volunteers AS v
+  ON a.vanid = v.vanid
+
+WHERE a.organizer IS NOT NULL
+)
