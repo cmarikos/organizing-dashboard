0348153ee366e16017d25435a57c3bc9b7689cda
Christina Marikos <christina.marikos@cta-tech.app>
2025-09-09 21:51:52 +0000

    update new vols

--- new_volunteers.sql
+++ new_volunteers.sql
@@ -1,9 +1,7 @@
-, new_volunteers AS(
-SELECT
-e.eventid
-, e.event_name
+SELECT DISTINCT
+e.vanid
+, c.codename AS organizer
 , MIN(EXTRACT(MONTH FROM e.start_date)) AS first_month
-, COUNT(DISTINCT e.vanid) AS volunteers
 FROM `proj-tmc-mem-mvp.everyaction_enhanced.enh_everyaction__events` AS e
 
 LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__eventscodes` AS ec
@@ -14,6 +12,8 @@ LEFT JOIN `proj-tmc-mem-mvp.everyaction_cleaned.cln_everyaction__codes` AS c
 
 WHERE e.role = 'Volunteer'
 AND e.final_status = 'Completed'
+AND c.codename IN ('Jhanitzel Bogarin','Leny Rivera','Tara Clayton','Hector Castellanos','Elyanna Juarez')
 
 GROUP BY 1,2
-)
+ORDER BY 3
+
